38fbf73bc6c8c926dc6ecfb366e0daaa731b21ef
Fredrik <simson@thesimson.net>
2018-11-17 11:13:08 +0900

    More test cases and compare.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Basic Architecture
-// Class Encoder 
+// Class Encoder
 // function DeleteAll
 // function Enroll
 // function Verify
@@ -11,8 +11,8 @@ extern crate crc;
 
 const LEN: usize = 32;
 pub trait Serialio {
-	fn readdata(&mut self, _len:u16) -> ( Vec<u8> );
-	fn writedata(&mut self, Vec<u8> ,_len:u32) ; 
+	fn readdata(&mut self, len:usize) -> ( Vec<u8> );
+	fn writedata(&mut self, Vec<u8> ,len:usize) ;
 }
 
 struct Encoder {
@@ -26,17 +26,32 @@ impl Encoder {
 	fn encode(&mut self) -> Vec<u8>  {
 		use crc::*;
 		//  [0x01,0x00,0x12,0x00]
-		// CRC is caclulated over transport layer bytes only
+		// CRC is caclulated over transport layer bytes only but little endian(sic!)
 		// , 0xb1, 0x2e, 0x45, 0x93
-		let transport = [0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00  ];
-		let crc = crc32::checksum_ieee(&transport);
+		// 0x93,0x45,0x2e,0xb1
+		let mut transport = vec![0,0,0,0,0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00];
+		let crc = crc32::checksum_ieee(&transport[4..]);
 		println!("{:x}",crc);
 
+		transport.push((0xFF& crc )as u8);
+		let crc  = crc/256;
+			transport.push((0xFF& crc )as u8);
+		let crc  = crc/256;
+			transport.push((0xFF& crc )as u8);
+		let crc  = crc/256;
+		transport.push((0xFF& crc )as u8);
 
+		//Add linklayer headers
+		transport[0]=0x1;
+		transport[1]=0x0;
+		transport[2]=0x12;
+		transport[3]=0x0;
+
+		let	len=transport.len();
+		self.myio.writedata(transport,len );
 		self.myio.readdata(4)
 	}
 }
-
 
 
 fn main() {
@@ -50,17 +65,15 @@ struct DummyInterface {
 }
 
 impl Serialio for DummyInterface {
-	fn readdata(&mut self, len:u16) -> Vec<u8> {
+	fn readdata(&mut self, len:usize) -> Vec<u8> {
 		let  buf  = self.readdatav.pop();
 		let  buf = buf.unwrap();
-		use std::usize;
 
-		assert!( len as usize == buf.len()) ;  // Did dummy provide the expected lenght of data
+		assert!( len == buf.len()) ;  // Did dummy provide the expected lenght of data
 		buf
 	}
-	fn writedata(&mut self,buffer:Vec<u8> ,_len:u32){
+	fn writedata(&mut self,buffer:Vec<u8> ,_len:usize){
 		self.writedatav.push(buffer)
-		
 	}
 
 }
@@ -76,15 +89,29 @@ mod tests {
 	#[test]
 	fn delete_all_templates() {
 		use super::*;
+		let vec1=vec![1,2,3];
+		let vec2=vec![1,2,3];
+		assert!(vec1 == vec2);
+
+
 		let mut dummy = DummyInterface::new();
 		dummy.readdatav.push(vec!(0,0,01,0));
 	    dummy.readdatav.push(vec!(0x3,0x7f,01,0x7f));
 		dummy.readdatav.push(vec!(0xff,0x7f,01,0x7f));
 
-		
+
 		let mut encoder = Encoder{internaldata: 0, myio: dummy};
 		encoder.encode();
-//		let dummy = encoder.myio;
+		let mut wb=encoder.myio.writedatav;
+		let tmp=wb.pop().unwrap();
+		let refvec:Vec<u8> = vec!(0x01,0x00,0x12,0x00,0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00,0xb1,0x2e,0x45,0x93);
+		assert!( tmp[4..]==refvec[4..]);
+		assert!( &tmp[0..4]==&refvec[0..4]);
+
+		let refvec:Vec<u8> = vec!(0x01,0x00,0x12,0x00,0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00,0xff,0xff,0xff,0xff);
+
+		assert!( &tmp != &refvec)
+
 	}
 	/*
 		#[test]
