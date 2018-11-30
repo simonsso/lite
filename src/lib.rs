dfba9d329f422a5af60214a3b1c1cb871ccbe84b
Fredrik Simonsson <simson@thesimson.net>
2018-11-30 23:06:23 +0900

    Transforming into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,83 @@
 // Basic Architecture
-// Class Encoder
+// Class BmLite
 // function DeleteAll
 // function Enroll
 // function Verify
 
-// have an halinterface
+//#![deny(missing_docs)]
+//#![deny(warnings)]
+#![feature(unsize)]
+#![no_std]
 
-#![allow(dead_code)]
+#![feature(alloc)]
+// Plug in the allocator crate
+
+extern crate alloc;
+use alloc::vec::Vec;
+
+extern crate embedded_hal;
 extern crate crc;
+extern crate byteorder;
 
-const LEN: usize = 32;
-pub trait Serialio {
-	fn readdata(&mut self, len:usize) -> ( Vec<u8> );
-	fn writedata(&mut self, Vec<u8> ,len:usize) ;
+#[macro_use(block)]
+
+extern crate nb;
+
+use embedded_hal::digital::{InputPin,OutputPin};
+use embedded_hal::blocking::spi::Transfer;
+use embedded_hal::spi::FullDuplex;
+use byteorder::{ByteOrder,BigEndian};
+
+pub struct BmLite<SPI,CS,RST,IRQ> {
+	spi: SPI,
+	cs: CS,
+    rst: RST,
+    irq: IRQ,
 }
 
-struct Encoder {
-	internaldata: i32,
-	myio: DummyInterface,
+pub enum Error<E>{
+    UnexpectedResponse,
+    Timeout,
+    HalErr(E),
 }
 
 
 
-impl Encoder {
-	fn encode(&mut self) -> Vec<u8>  {
+impl <SPI,CS,RST,IRQ, E> BmLite<SPI,CS,RST,IRQ>
+where  SPI: Transfer<u8, Error = E>,
+    SPI: FullDuplex<u8, Error = E>,
+	CS: OutputPin,
+    RST: OutputPin,
+    IRQ: InputPin
+{
+	    /// Creates a new driver from an SPI peripheral and a chip select
+    /// digital I/O pin.
+    pub fn new(spi: SPI, cs: CS, rst: RST, irq: IRQ) -> Self {
+        let en= BmLite { spi: spi, cs: cs, rst: rst, irq: irq };
+
+        en
+    }
+	pub fn reset(&mut self) -> Result<u8, Error<E>>  {
+        self.rst.set_low();
+        //ToDo add a delay here.
+        self.rst.set_high();
+        //ToDoReset internal data strutures when they exist
+        Ok(0)
+    }
+	pub fn delete_all(&mut self) -> Result<u8, Error<E>>  {
 		use crc::*;
 		//  [0x01,0x00,0x12,0x00]
 		// CRC is caclulated over transport layer bytes only but little endian(sic!)
 		// , 0xb1, 0x2e, 0x45, 0x93
 		// 0x93,0x45,0x2e,0xb1
-		let mut transport = vec![0,0,0,0,0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00];
-		let crc = crc32::checksum_ieee(&transport[4..]);
-		println!("{:x}",crc);
+		let mut transport:Vec<u8> = [0,0,0,0,0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00].to_vec();
 
+		let crc = crc32::checksum_ieee(&transport[4..]);
 		transport.push((0xFF& crc )as u8);
 		let crc  = crc/256;
-			transport.push((0xFF& crc )as u8);
+		transport.push((0xFF& crc )as u8);
 		let crc  = crc/256;
-			transport.push((0xFF& crc )as u8);
+		transport.push((0xFF& crc )as u8);
 		let crc  = crc/256;
 		transport.push((0xFF& crc )as u8);
 
@@ -47,16 +87,61 @@ impl Encoder {
 		transport[2]=0x12;
 		transport[3]=0x0;
 
-		let	len=transport.len();
-		self.myio.writedata(transport,len );
-		self.myio.readdata(4)
+        self.cs.set_low();
+        let _ans = self.spi.transfer( &mut transport).map_err(Error::HalErr)?;
+        self.cs.set_high();
+
+        while self.irq.is_low(){
+        }
+        self.cs.set_low();
+        let mut ack:Vec<u8> = [0,0,0,0].to_vec();
+        let ack = self.spi.transfer(&mut ack).map_err(Error::HalErr)?;
+        self.cs.set_high();
+
+        // expect magic 7f ff 01 7f
+        if ! (ack[0] == 0x7f && ack[1] == 0xff && ack[2] == 0x01 && ack[3] == 0x7f ) {
+            return Err(Error::UnexpectedResponse)
+        }
+        while self.irq.is_low(){
+        }
+        self.cs.set_low();
+        let mut v0:Vec<u8> = [0,0,0,0].to_vec();
+        let v0 = self.spi.transfer(&mut v0).map_err(Error::HalErr)?;
+        self.cs.set_high();
+        if ! (v0[0] == 0 && v0[1] == 0xf && v0[2] == 0x01 && v0[3] == 0x7f ) {
+         //handle error here should be 0 0 size 0 or something
+            //return Err(());
+        }
+        let xxx:usize = 4 + v0[2] as usize;
+        let mut v:Vec<u8> = Vec::with_capacity(xxx);
+        self.cs.set_low();
+        for _i in 0..xxx {
+           let _ans=block!(self.spi.send(0)).map_err(Error::HalErr)?;
+           let ans:u8 = block!(self.spi.read()).map_err(Error::HalErr)?;
+           v.push(ans);
+        }
+        self.cs.set_high();
+		let crc = crc32::checksum_ieee(&v[0..xxx-4]);
+
+        if crc == BigEndian::read_u32(&v[xxx-4..xxx]){
+            self.cs.set_low();
+            let mut ack:Vec<u8> = [0x7f,0xff,0x01,0x7f].to_vec();
+            let mut ack = self.spi.transfer(&mut ack).map_err(Error::HalErr)?;
+            self.cs.set_high();
+            return Ok(1);
+        }else {
+            //crc error
+            return Ok(99);
+        }
+
+        Ok(0)
 	}
 }
 
-
+/*
 fn main() {
 	let mut dummy = DummyInterface::new();
-	let mut encoder = Encoder{internaldata: 0, myio: dummy};
+	let mut encoder = BmLite{internaldata: 0, myio: dummy};
 	encoder.encode();
 }
 struct DummyInterface {
@@ -77,14 +162,14 @@ impl Serialio for DummyInterface {
 	}
 
 }
+*/
+#[cfg(test)]
 impl DummyInterface{
 	fn new() -> Self{
 		DummyInterface {
 		readdatav: vec!(),writedatav: vec!() }
 	}
 }
-
-#[cfg(test)]
 mod tests {
 	#[test]
 	fn delete_all_templates() {
@@ -100,7 +185,7 @@ mod tests {
 		dummy.readdatav.push(vec!(0xff,0x7f,01,0x7f));
 
 
-		let mut encoder = Encoder{internaldata: 0, myio: dummy};
+		let mut encoder = BmLite{internaldata: 0, myio: dummy};
 		encoder.encode();
 		let mut wb=encoder.myio.writedatav;
 		let tmp=wb.pop().unwrap();
