cf16b95ebcc1d3b72b07e2d949592156ebcd36c7
Fredrik Simonsson <simson@thesimson.net>
2018-12-03 17:35:25 +0900

    MVP two working commands

--- src/bmlitelib.rs
+++ src/bmlitelib.rs
@@ -75,7 +75,18 @@ where  SPI: Transfer<u8, Error = E>,
         Ok(0)
     }
     fn link(&mut self, appldata:Vec<u8> ) ->  Result<(Vec<u8>), Error<E>> {
-		let mut transport:Vec<u8> = [0,0,0,0,0x0c,0x00,0x01,0x00,0x01,0].to_vec();
+        //                           Ch   size size      seqnum     seqlen
+		let mut transport:Vec<u8> = [0,0, 0,0, 0x0,0x00, 0x01,0x00,0x01,0].to_vec();
+		//Add linklayer headers
+        let len = appldata.len() as u32;
+		transport[0]=0x1;   //Chanel
+		transport[1]=0x0;
+		transport[2]=(len & 0xFF) as u8 +6 ; // Size
+		transport[3]=0x0; // MSB always 0
+		transport[4]=(len & 0xFF) as u8  ;   // Size
+		transport[5]=0x0; // MSB always 0
+    
+
         transport.extend(appldata.iter());
 		use crc::*;
 		let crc = crc32::checksum_ieee(&transport[4..]);
@@ -86,12 +97,6 @@ where  SPI: Transfer<u8, Error = E>,
 		transport.push((0xFF& crc )as u8);
 		let crc  = crc/256;
 		transport.push((0xFF& crc )as u8);
-
-		//Add linklayer headers
-		transport[0]=0x1;
-		transport[1]=0x0;
-		transport[2]=0x12;
-		transport[3]=0x0;
 
         self.cs.set_low();
         let _ans = self.spi.transfer( &mut transport).map_err(Error::HalErr)?;
@@ -154,8 +159,9 @@ where  SPI: Transfer<u8, Error = E>,
         // v[8:9] CMD should be same as CMD sent.
         Ok(v.split_off(6))
     }
-	pub fn delete_all(&mut self) -> Result<u8, Error<E>>  {
-		let mut transport:Vec<u8> = [0x02, 0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00].to_vec();
+	pub fn get_version(&mut self) -> Result<u8, Error<E>>  {
+        //                           CMD_INFO   Size      GET1004  , NulNul
+		let mut transport:Vec<u8> = [0x04, 0x30,0x01,0x00,0x04,0x10, 0,0].to_vec();
         let cmd = (transport[1],transport[0]);
         let resp=self.link(transport)?;
 
@@ -174,7 +180,48 @@ where  SPI: Transfer<u8, Error = E>,
         if as_u16(resp[3],resp[2]) != SensorResp::ARG_Result as u16 {
              return Err(Error::UnexpectedResponse)
         }
-        let resp_len = as_u16(resp[4],resp[5]);
+        Ok(0)
+    }
+	pub fn capture(&mut self) -> Result<u8, Error<E>>  {
+        //                           CMD_Capure   aNum
+		let mut transport:Vec<u8> = [0x01, 0x00, 0x0,0x0].to_vec();
+        let cmd = (transport[1],transport[0]);
+        let resp=self.link(transport)?;
+
+
+        if resp.len() <6 {
+             // expect at lease some data here
+             return Err(Error::UnexpectedResponse)
+        }
+        let resp_len = as_u16(resp[2],resp[3]);
+        if resp_len ==1 && as_u16(resp[5],resp[4]) == SensorResp::ARG_Result as u16 {
+            return Ok(resp[7])
+        }
+        Err(Error::UnexpectedResponse)
+        
+    }
+	pub fn delete_all(&mut self) -> Result<u8, Error<E>>  {
+        //                           TmplStoreage  aNum   Delete    NulNul    ARGALL     NulNul 
+		let mut transport:Vec<u8> = [0x02, 0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00].to_vec();
+        let cmd = (transport[1],transport[0]);
+        let resp=self.link(transport)?;
+
+
+        if resp.len() <6 {
+             // expect at lease some data here
+             return Err(Error::UnexpectedResponse)
+        }
+        if cmd != (resp[1],resp[0]){
+             // command response did not match command.
+             return Err(Error::UnexpectedResponse)
+        }
+        // expected data len = 1
+        //          Result == ARG_Result
+        // val ==1 
+        if as_u16(resp[5],resp[4]) != SensorResp::ARG_Result as u16 {
+             return Err(Error::UnexpectedResponse)
+        }
+        let resp_len = as_u16(resp[2],resp[3]);
 
         Ok(resp[7])
 	}
